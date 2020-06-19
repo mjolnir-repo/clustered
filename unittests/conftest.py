e09d6ca994a349612152b9fc58aff21a44319827
Saumalya Sarkar <potter@Saumalyas-MacBook-Air.local>
2020-06-19 20:40:06 +0530

    follows semi-singletone methodology. Configurations will be refreshed only when any config_file is passed as input or explicitly refresh option is enabled. Rest of the time already extracted configuration will be returned.

--- unittests/conftest.py
+++ unittests/conftest.py
@@ -19,48 +19,17 @@ def _test_file_dir():
 
 
 ############################## Engine :: Configuration Engine :: Environment Configuration ##############################
-@pytest.fixture(name="environment_configuration_filename", scope="module")
-def _environment_configuration_filename():
-    """Test environment config file name."""
-    return 'dummy_environment_configuration_filename.json'
-
-@pytest.fixture(name="environment_configuration", scope="module")
-def _environment_configuration():
-    """Test environment config."""
-    return {
-        "DB_ENGINE": "sqlite",
-        "DB_FILE": "/Volumes/WorkSpace/POC/Mjolnir/clustered/clustered/database/clustered.db",
-    }
-
-@pytest.yield_fixture(name="environment_configuration_file", scope="module", autouse=True)
-def _environment_configuration_file(test_file_dir, environment_configuration_filename, environment_configuration):
-    """ 
-        Creating JSON file to test environment configuration access.
-        The file will be removed once test is completed as part of teardown process.
-    """
-    try:
-        environment_configuration_file = os.path.join(test_file_dir, environment_configuration_filename)
-        with open(environment_configuration_file, 'w') as f:
-            json.dump(environment_configuration, f)
-        yield environment_configuration_file
-        os.remove(environment_configuration_file)
-    except Exception as e:
-        print("~" * 100)
-        traceback.print_exc(file=sys.stdout)
-        print("~" * 100)
-        assert False
-
 @pytest.fixture(name="default_environment_configuration_filename", scope="module")
 def _default_environment_configuration_filename():
     """Test with default environment config file name."""
     return 'Environment_Config.json'
 
 @pytest.fixture(name="default_environment_configuration", scope="module")
-def _default_environment_configuration():
+def _default_environment_configuration(test_file_dir):
     """Test default environment config."""
     return {
         "DB_ENGINE": "sqlite",
-        "DB_FILE": "/Volumes/WorkSpace/POC/Mjolnir/clustered/clustered/database/default_clustered.db",
+        "DB_FILE": os.path.join(test_file_dir, "default_clustered.db"),
     }
 
 @pytest.yield_fixture(name="default_environment_configuration_file", scope="module", autouse=True)
@@ -86,38 +55,6 @@ def _default_environment_configuration_file(default_environment_configuration_fi
 
 
 ############################## Engine :: Configuration Engine :: Repository Configuration ##############################
-@pytest.fixture(name="repository_configuration_filename", scope="module")
-def _repository_configuration_filename():
-    """Test repository config file name."""
-    return 'dummy_repository_configuration_filename.json'
-
-@pytest.fixture(name="repository_configuration", scope="module")
-def _repository_configuration():
-    """Test repository config."""
-    return {
-        "AWS_ACCESS_KEY_ID": "TEST",
-        "AWS_SECRET_ACCESS_KEY": "TEST",
-        "AWS_DEFAULT_REGION": "TEST"
-    }
-
-@pytest.yield_fixture(name="repository_configuration_file", scope="module", autouse=True)
-def _repository_configuration_file(test_file_dir, repository_configuration_filename, repository_configuration):
-    """ 
-        Creating JSON file to test repository configuration access.
-        The file will be removed once test is completed as part of teardown process.
-    """
-    try:
-        repository_configuration_file = os.path.join(test_file_dir, repository_configuration_filename)
-        with open(repository_configuration_file, 'w') as f:
-            json.dump(repository_configuration, f)
-        yield repository_configuration_file
-        os.remove(repository_configuration_file)
-    except Exception as e:
-        print("~" * 100)
-        traceback.print_exc(file=sys.stdout)
-        print("~" * 100)
-        assert False
-
 @pytest.fixture(name="default_repository_configuration_filename", scope="module")
 def _default_repository_configuration_filename():
     """Test with default repository config file name."""
@@ -127,9 +64,7 @@ def _default_repository_configuration_filename():
 def _default_repository_configuration():
     """Test default repository config."""
     return {
-        "AWS_ACCESS_KEY_ID": "dEFAULTfILEtEST",
-        "AWS_SECRET_ACCESS_KEY": "TEST",
-        "AWS_DEFAULT_REGION": "TEST"
+        "CLOUD_TYPE": "AWS-DEFAULT"
     }
 
 @pytest.yield_fixture(name="default_repository_configuration_file", scope="module", autouse=True)
